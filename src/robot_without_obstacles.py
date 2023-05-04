b2cca46a77bbf7ba5353bf13ad91f47fa843a289
Benjamin Dayres <benjamin.dayres@enseirb-matmeca.fr>
2023-05-04 12:12:29 +0200

    angle management

--- src/robot_without_obstacles.py
+++ src/robot_without_obstacles.py
@@ -2,6 +2,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import math
 from tsp import tsp_brute_force
+from load import load
 
 def get_edge_from_perm(perm):
     l = []
@@ -9,7 +10,7 @@ def get_edge_from_perm(perm):
         l += [edge]
     return l
 
-def show_robot_path(coords):
+def show_robot_path(coords, C=0):
     # Créer un graphe complet avec des poids égaux à la distance euclidienne entre les nœuds
     n = len(coords)
     G = nx.complete_graph(n)
@@ -17,8 +18,9 @@ def show_robot_path(coords):
         dist = math.sqrt((coords[u][0]-coords[v][0])**2 + (coords[u][1]-coords[v][1])**2)
         w['weight'] = dist
 
-    min_perm = tsp_brute_force(G)
+    min_perm = tsp_brute_force(G, coords, C)
     min_edges = get_edge_from_perm(min_perm)
+    print(f"Best path : {min_perm}")
 
     H = nx.Graph()
     H.add_edges_from(min_edges)
@@ -27,12 +29,12 @@ def show_robot_path(coords):
     pos = dict(zip(range(n), coords))
     nx.draw_networkx_nodes(G, pos, node_color='lightblue')
     nx.draw_networkx_labels(G, pos)
-    nx.draw_networkx_edges(G, pos, width=1.0, edge_color='grey')
-    nx.draw_networkx_edges(H, pos, width=2.0, edge_color='black')
+    nx.draw_networkx_edges(G, pos, width=0.5, edge_color='grey')
+    nx.draw_networkx_edges(H, pos, width=2.0, edge_color='blue')
     plt.axis('off')
     plt.grid(True)
     plt.show()
 
 # Créer une liste de coordonnées de nœuds
 coords = [(0, 0), (1, 2), (3, 1), (2, 4), (4, 3), (2, 2), (3, 3)]
-show_robot_path(coords)
+show_robot_path(load("data.txt")[1], 0)
--- src/tsp.py
+++ src/tsp.py
@@ -1,17 +1,52 @@
 import itertools as it
 import networkx as nx
+import numpy as np
+import math
+
+def check_same_direction(v1, v2):
+    x1, y1 = v1
+    x2, y2 = v2
+    if (x1 > 0 and x2 > 0) or (x1 < 0 and x2 < 0):
+        if (y1 > 0 and y2 > 0) or (y1 < 0 and y2 < 0):
+            return True
+    elif (y1 > 0 and y2 > 0) or (y1 < 0 and y2 < 0):
+        if (x1 > 0 and x2 > 0) or (x1 < 0 and x2 < 0):
+            return True
+    return False
+
+
+def angle_between_vectors(u, v):
+    if u[0]*v[1] == u[1]*v[0]:
+        return 0 if check_same_direction(u, v) else math.pi
+    dot_product = np.dot(u, v)
+    norm_u = np.linalg.norm(u)
+    norm_v = np.linalg.norm(v)
+    cos_theta = dot_product / (norm_u * norm_v)
+    theta = np.arccos(cos_theta)
+    return theta
+
+def compute_angle_wait(perm, pos, C):
+    current_vec = (0,1)
+    wait = 0
+    for (u, v) in zip(perm[:-1], perm[1:]):
+        next_vec = (pos[v][0] - pos[u][0], pos[v][1] - pos[u][1])
+        angle = angle_between_vectors(current_vec, next_vec)
+        wait += angle * C
+        current_vec = next_vec
+    return wait
 
 def iter_path(G):
     start = list(G.nodes())[0]
     nodes = list(G.nodes())[1:]
     for p in it.permutations(nodes):
-        yield [start] + list(p) + [start]
+        yield [start] + list(p)
 
-def tsp_brute_force(G):
+def tsp_brute_force(G, pos, C):
     min_dist = 10e30
     ret = None
     for p in iter_path(G):
         length = 0
+        length += compute_angle_wait(p, pos, C)
         for (u, v) in zip(p[:-1], p[1:]):
             length += G.get_edge_data(u, v)["weight"]
         if length < min_dist:
